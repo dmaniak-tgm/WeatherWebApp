4cc3efeeed3975d3675ecb686b013cef8975c11b
MANIAK David <U11Y10L@post.at>
2018-07-05 11:22:04 +0200

    removed onclick

--- src/City.tsx
+++ src/City.tsx
@@ -19,19 +19,12 @@ export default class City extends React.Component<ICityProps, ICityState> {
         }
     }
 
-    public detailsCity = () => {
-        // this.setState({
-        //     citystate: this.props.name + (new Date).toUTCString(),
-        // })
-        alert('Click works');
-    }
-
     public render() {
         return (
             <div className='col-sm-4 ' onClick={() => { this.props.activateDetails(this.state.citystate) }}>
                 <div className="card ">
                     <img className='card-img-top' src={this.props.src} alt={this.props.src} />
-                    <div className="card-body cBody" onClick={this.detailsCity}>
+                    <div className="card-body cBody">
                         <h3 className="card-title display-4">{this.state.citystate}</h3>
                     </div>
                 </div>
