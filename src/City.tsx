29f69168d55d3ff2879f559cab26fcf03a6674ae
MANIAK David <U11Y10L@post.at>
2018-07-06 12:06:51 +0200

    refactoring

--- src/City.tsx
+++ src/City.tsx
@@ -4,28 +4,23 @@ import './City.css';
 export interface ICityProps {
     name: string;
     src: string;
-    activateDetails: (city: string, cityURL: string) => void
+    activateDetails: (city: string, cityURL: string) => void;
 }
-
-interface ICityState {
-    citystate: string;
-}
+interface ICityState {}
 
 export default class City extends React.Component<ICityProps, ICityState> {
     constructor(props: ICityProps) {
         super(props);
-        this.state = {
-            citystate: props.name
-        }
     }
 
     public render() {
-        return (
-            <div className='col-sm-4 ' onClick={() => { this.props.activateDetails(this.state.citystate, this.props.src) }}>
-                <div className="card ">
-                    <img className='card-img-top' src={this.props.src} alt={this.props.src} />
+        return(
+            <div className="col-sm-4" 
+                 onClick={() => this.props.activateDetails(this.props.name, this.props.src)}>
+                <div className="card">
+                    <img className="card-img-top" src={this.props.src} alt={this.props.src} />
                     <div className="card-body cBody">
-                        <h3 className="card-title display-4">{this.state.citystate}</h3>
+                        <h3 className="card-title display-4">{this.props.name}</h3>
                     </div>
                 </div>
             </div>
